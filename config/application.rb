7f06ac2f3451827876607bd0aaebb66dee2652bd
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2019-09-23 12:34:38 +0100

    Added the school and student models

--- config/application.rb
+++ config/application.rb
@@ -30,4 +30,8 @@ module Prospectus
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
+
+  def self.credentials
+    @credentials ||= Rails.application.credentials[Rails.env.to_sym]
+  end
 end
